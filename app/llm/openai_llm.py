6ad828b3e796c937d56187bd4a93970eb925b02b
Saksham Dahake <sakshamdahake@192.168.1.12>
2025-11-22 11:58:20 +0530

    Let's use a cheaper but better model

--- app/llm/openai_llm.py
+++ app/llm/openai_llm.py
@@ -8,24 +8,6 @@ def get_llm():
     return ChatOpenAI(
     base_url="https://ai.megallm.io/v1",
     api_key=os.getenv("OPENAI_API_KEY"),
-    model="openai-gpt-oss-20b",
+    model="openai-gpt-oss-120b",
     temperature = 0.0,
 )
-
-# if __name__ == "__main__":
-#     from langchain_core.messages import HumanMessage
-    
-#     llm = get_llm()
-#     print("LLM object created:", llm)
-
-#     prompt = "thank saksham for his good work"
-#     print("\nSending test request to LLM...")
-    
-#     try:
-#         result = llm.invoke([HumanMessage(content=prompt)])
-#         print("\n--- LLM RAW RESPONSE ---")
-#         print(result.content)
-#         print("------------------------")
-#     except Exception as e:
-#         print("\n⚠️ LLM invocation failed!")
-#         print("Error:", e)
